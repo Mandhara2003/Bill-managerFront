273e6f6ea7ad3d6f1d8dfc61eca5d13f3f72bc72
mandhara <hmandhara45@gmail.com>
2023-11-29 15:15:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BillingSystem from './BillingSystem'; // Import the BillingSystem component
+
+function App() {
+  return (
+    <div className="App">
+      <BillingSystem />
+    </div>
+  );
+}
+
+export default App;
